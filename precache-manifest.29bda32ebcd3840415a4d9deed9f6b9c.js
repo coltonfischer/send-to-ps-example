69376976c3ae56769eaa71f2f7b25594b6a8be94
Colton Fischer (via Travis CI) <coltonfischer@ymail.com>
2019-11-19 18:07:51 +0000

    Deploy coltonfischer/send-to-ps-example to github.com/coltonfischer/send-to-ps-example.git:gh-pages

---
+++ precache-manifest.29bda32ebcd3840415a4d9deed9f6b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d747414436946167370b1683c3d54a75",
+    "url": "/send-to-ps-example/index.html"
+  },
+  {
+    "revision": "277e581209eceeee4589",
+    "url": "/send-to-ps-example/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b82391b87428e44e3a60",
+    "url": "/send-to-ps-example/static/js/2.292ee004.chunk.js"
+  },
+  {
+    "revision": "277e581209eceeee4589",
+    "url": "/send-to-ps-example/static/js/main.b5471660.chunk.js"
+  },
+  {
+    "revision": "27da4a75cd2f3374faa3",
+    "url": "/send-to-ps-example/static/js/runtime-main.6a407fdf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/send-to-ps-example/static/media/logo.25bf045c.svg"
+  }
+]);
